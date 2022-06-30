245f422f2beb316a4679d4b69e9ac4203a9c71c8
brayangomez08 <gomezmancobrayanalexander@gmail.com>
2022-06-30 08:16:33 -0500

    Animations and the scroll to go up the page are added

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,3 +16,42 @@ if (navClose) {
 		navMenu.classList.remove('show-menu');
 	});
 }
+
+/*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll('.nav__link');
+
+function linkAction() {
+	const navMenu = document.getElementById('nav-menu');
+	navMenu.classList.remove('show-menu');
+}
+navLink.forEach((n) => n.addEventListener('click', linkAction));
+
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+function scrollHeader() {
+	const header = document.getElementById('header');
+	if (this.scrollY >= 50) header.classList.add('scroll-header');
+	else header.classList.remove('scroll-header');
+}
+window.addEventListener('scroll', scrollHeader);
+
+/*=============== SHOW SCROLL UP ===============*/
+function scrollUp() {
+	const scrollUp = document.getElementById('scroll-up');
+	if (this.scrollY >= 260) scrollUp.classList.add('show-scroll');
+	else scrollUp.classList.remove('show-scroll');
+}
+window.addEventListener('scroll', scrollUp);
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+	origin: 'top',
+	distance: '60px',
+	duration: 2500,
+	delay: 400,
+	// reset: true
+});
+
+sr.reveal(`.home__container`);
+sr.reveal(`.footer__content`, { interval: 100 });
+sr.reveal(`.about__intro`, { origin: 'left' });
+sr.reveal(`.about__content`, { origin: 'right' });
